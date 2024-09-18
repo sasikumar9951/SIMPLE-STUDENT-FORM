c6bbeb544bac90809890bbf798adde3a7c6d59c6
sasikumar9951 <sasikumar.s9951@gmail.com>
2024-09-18 20:37:00 +0530

    Add files via upload
    THIS IS MY SIMPLE STUDENT FORM CODE

---
+++ java.js
@@ -0,0 +1,51 @@
+var int = document.getElementById("int1");
+var int2=document.getElementById("int2")
+var int3=document.getElementById("int3")
+var int5=document.getElementById("int5")
+var int6=document.getElementById("int6")
+
+
+
+
+var row=document.getElementById("row")
+function create(event) {
+        event.preventDefault();
+
+    const listitem = document.createElement("td");
+    listitem.textContent = int.value
+    row.append(listitem)
+
+
+    const listitem2 = document.createElement("td");
+    listitem2.textContent = int2.value;
+    row.append(listitem2);
+
+    const listitem3 = document.createElement("td");
+    listitem3.textContent = int3.value;
+    row.append(listitem3);
+
+       
+
+    const listitem4 = document.createElement("td");
+    listitem4.textContent = int5.value;
+    row.append(listitem4);
+
+    const listitem5 = document.createElement("td");
+    listitem5.textContent = int6.value;
+    row.append(listitem5);
+
+    const listitem6=document.createElement("td")
+    listitem6.innerHTML="<button onclick=saya(event)>Delete</button>"
+    row.append(listitem6)
+
+  
+}
+
+function saya(event){
+    row.remove()
+}
+
+
+
+
+
